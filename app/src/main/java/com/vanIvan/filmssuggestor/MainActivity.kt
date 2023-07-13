a5da8d3076d517e73292214db1dfa5c8183bdc78
Ivan Seregin <vanIvan4.work@gmail.com>
2023-07-14 04:59:22 +0500

    Add functionality

--- app/src/main/java/com/vanIvan/filmssuggestor/MainActivity.kt
+++ app/src/main/java/com/vanIvan/filmssuggestor/MainActivity.kt
@@ -1,32 +1,38 @@
 package com.vanIvan.filmssuggestor
 
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.text.method.LinkMovementMethod
+import android.os.Handler
+import android.os.Looper
 import android.view.Menu
-import android.widget.TextView
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.navigation.NavigationView
 import com.vanIvan.filmssuggestor.databinding.ActivityMainBinding
+import com.vanIvan.filmssuggestor.ui.home.HomeFragment
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
+import org.json.JSONObject
 import java.io.IOException
+import java.util.concurrent.Executors
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-
-    private var client = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,11 +51,13 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+        println(R.id.nav_home)
         appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.nav_home, R.id.nav_archive, R.id.nav_info), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-        AsynchronousGet().run("http://worldtimeapi.org/api/ip")
+        run("http://worldtimeapi.org/api/ip")
+//        generateIMDBid()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -63,16 +71,11 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
-}
-
-class AsynchronousGet {
     private val client = OkHttpClient()
-
     fun run(url: String) {
         val request = Request.Builder()
             .url(url)
             .build()
-
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
                 e.printStackTrace()
@@ -91,4 +94,95 @@ class AsynchronousGet {
             }
         })
     }
+//
+//    fun runMovie(url: String) {
+//        val request = Request.Builder()
+//            .url(url)
+//            .build()
+//        var res: String? = ""
+//        client.newCall(request).enqueue(object : Callback {
+//            override fun onFailure(call: Call, e: IOException) {
+//                e.printStackTrace()
+//            }
+//
+//            override fun onResponse(call: Call, response: Response) {
+//                response.use {
+//                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+//
+//                    for ((name, value) in response.headers) {
+//                        println("$name: $value")
+//                    }
+//
+//                    res = response.body?.string()
+//                    var json = JSONObject(res)
+//                    if(json.has("Error")) {
+//                        println("Unexpected error, trying again...")
+//                        Thread.sleep(1_000)
+//                        generateIMDBid()
+//                        return
+//                    }
+//                    println(json)
+//                    println(R.id.nav_home)
+//                    runOnUiThread(Runnable {
+////                        setContentView(R.layout.activity_main)
+//                        println(R.id.nav_home)
+//                        println(supportFragmentManager.fragments[0].tag)
+//                        val fragment: HomeFragment? =
+//                            supportFragmentManager.findFragmentById(R.id.nav_home) as HomeFragment?
+//                        val executor = Executors.newSingleThreadExecutor()
+//                        val handler = Handler(Looper.getMainLooper())
+//                        var image: Bitmap? = null
+//                        executor.execute{
+//
+//                            val imageUrl = json.getString("Poster")
+//                            val title = json.getString("Title")
+//                            val genre = json.getString("Genre")
+//                            val plot = json.getString("Plot")
+//                            if(imageUrl == "N\\/A") {
+//                                Thread.sleep(1_000)
+//                                generateIMDBid()
+//                                executor.shutdown()
+//                            }
+//                            try {
+//                                val `in` = java.net.URL(imageUrl).openStream()
+//                                image = BitmapFactory.decodeStream(`in`)
+//                                handler.post{
+//                                    fragment?.updateText(image, title, genre, plot)
+//                                    println(fragment)
+//                                    if(fragment != null) {
+//                                        val ft: FragmentTransaction =
+//                                            supportFragmentManager.beginTransaction()
+//                                        ft.detach(fragment as Fragment)
+//                                        ft.attach(fragment as Fragment)
+//                                        ft.commit()
+//                                    }
+//                                }
+//                            }
+//                            catch (e:java.lang.Exception) {
+//                                e.printStackTrace()
+//                            }
+//                            fragment?.updateText(null, title, genre, plot)
+//                            println(fragment)
+//                            if(fragment != null) {
+//                                val ft: FragmentTransaction =
+//                                    supportFragmentManager.beginTransaction()
+//                                ft.detach(fragment as Fragment)
+//                                ft.attach(fragment as Fragment)
+//                                ft.commit()
+//                            }
+//                        }
+//                    })
+//                }
+//            }
+//        })
+//    }
+//
+//    fun generateIMDBid() {
+//        val api_key = "9cc1ed67"
+//        val rnds = (1..7000000).random()
+//
+//        val url = "http://www.omdbapi.com/?i=tt${rnds}&apikey=${api_key}"
+//        println(url)
+//        runMovie(url)
+//    }
 }
--- app/src/main/java/com/vanIvan/filmssuggestor/ui/home/HomeFragment.kt
+++ app/src/main/java/com/vanIvan/filmssuggestor/ui/home/HomeFragment.kt
@@ -1,13 +1,31 @@
 package com.vanIvan.filmssuggestor.ui.home
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.vanIvan.filmssuggestor.R
 import com.vanIvan.filmssuggestor.databinding.FragmentHomeBinding
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONObject
+import java.io.IOException
+import java.io.InputStream
+import java.net.MalformedURLException
+import java.net.URL
+import java.util.concurrent.Executors
+
 
 class HomeFragment : Fragment() {
 
@@ -17,6 +35,10 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private var imageView: ImageView? = null
+    private var titleView: TextView? = null
+    private var genreView: TextView? = null
+    private var plotView: TextView? = null
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -27,7 +49,18 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        println(R.id.imageView2)
+        imageView = view?.findViewById<ImageView>(R.id.imageView2)
+        titleView = view?.findViewById<TextView>(R.id.titleView)
+        genreView = view?.findViewById<TextView>(R.id.genreView)
+        plotView = view?.findViewById<TextView>(R.id.plotView)
 
+        val api_key = "9cc1ed67"
+        val rnds = (1..7000000).random()
+
+        val url = "http://www.omdbapi.com/?i=tt${rnds}&apikey=${api_key}"
+        runMovie(url)
+        println("Recreating view...")
         val textView: TextView = binding.textHome
 //        homeViewModel.text.observe(viewLifecycleOwner) {
 //            textView.text = it
@@ -39,4 +72,91 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    public fun updateText(binaryImage: Bitmap?, title: String, genre: String, plot: String) {
+
+        view?.findViewById<TextView>(R.id.titleView)?.text = title
+        view?.findViewById<TextView>(R.id.genreView)?.text = "Genres: $genre"
+        view?.findViewById<TextView>(R.id.plotView)?.text = plot
+//        imageView?.setImageBitmap(binaryImage)
+//        titleView?.text = title
+//        genreView?.text = "Genres: $genre"
+//        plotView?.text = plot
+
+    }
+    private val client = OkHttpClient()
+    fun runMovie(url: String) {
+        val request = Request.Builder()
+            .url(url)
+            .build()
+        var res: String? = ""
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                    for ((name, value) in response.headers) {
+                        println("$name: $value")
+                    }
+
+                    res = response.body?.string()
+                    var json = JSONObject(res)
+                    if(json.has("Error")) {
+                        println("Unexpected error, trying again...")
+                        Thread.sleep(1_000)
+                        generateIMDBid()
+                        return
+                    }
+                    println(json)
+
+                        val executor = Executors.newSingleThreadExecutor()
+                        val handler = Handler(Looper.getMainLooper())
+                        var image: Bitmap? = null
+                        executor.execute{
+
+                            val imageUrl = json.getString("Poster")
+                            val title = json.getString("Title")
+                            val genre = json.getString("Genre")
+                            val plot = json.getString("Plot")
+                            if(imageUrl == "N\\/A") {
+                                Thread.sleep(1_000)
+                                generateIMDBid()
+                                executor.shutdown()
+                            }
+                            try {
+                                val `in` = java.net.URL(imageUrl).openStream()
+                                image = BitmapFactory.decodeStream(`in`)
+                                handler.post{
+                                    runOnUiThread {
+                                        view?.findViewById<ImageView>(R.id.imageView2)?.setImageBitmap(image)
+                                    }
+                                }
+                            }
+                            catch (e:java.lang.Exception) {
+                                e.printStackTrace()
+                            }
+                            updateText(null, title, genre, plot)
+                        }
+                }
+            }
+        })
+    }
+
+    fun generateIMDBid() {
+        val api_key = "9cc1ed67"
+        val rnds = (1..7000000).random()
+
+        val url = "http://www.omdbapi.com/?i=tt${rnds}&apikey=${api_key}"
+        println(url)
+        runMovie(url)
+    }
+fun Fragment?.runOnUiThread(action: () -> Unit) {
+    this ?: return
+    if (!isAdded) return // Fragment not attached to an Activity
+    activity?.runOnUiThread(action)
+}
 }
