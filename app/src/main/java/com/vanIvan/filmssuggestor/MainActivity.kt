0baee7b3cd5abbcf3c6c9e2fee5bbb939e608bfd
Ivan Seregin <vanIvan4.work@gmail.com>
2023-07-13 18:37:05 +0500

    Add info and links

--- app/src/main/java/com/vanIvan/filmssuggestor/MainActivity.kt
+++ app/src/main/java/com/vanIvan/filmssuggestor/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.vanIvan.filmssuggestor
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.Menu
+import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -12,11 +14,19 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.vanIvan.filmssuggestor.databinding.ActivityMainBinding
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+    private var client = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,9 +46,19 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_archive, R.id.nav_search), drawerLayout)
+                R.id.nav_home, R.id.nav_archive, R.id.nav_info), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        // Declaring and Initializing
+        // the TextView from the layout file
+        setContentView(R.layout.fragment_info)
+        val mTextView = findViewById<TextView>(R.id.text_view_1)
+
+        // Finding and displaying the content
+        // that consists a URL as a hyperlink
+        mTextView.movementMethod = LinkMovementMethod.getInstance()
+        setContentView(binding.root)
+        AsynchronousGet().run("http://worldtimeapi.org/api/ip")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -50,5 +70,34 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+}
+
+class AsynchronousGet {
+    private val client = OkHttpClient()
+
+    fun run(url: String) {
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                    for ((name, value) in response.headers) {
+                        println("$name: $value")
+                    }
+
+                    println(response.body?.string())
+                }
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/vanIvan/filmssuggestor/ui/info/InfoViewModel.kt
@@ -0,0 +1,7 @@
+package com.vanIvan.filmssuggestor.ui.info
+
+import androidx.lifecycle.ViewModel
+
+class InfoViewModel : ViewModel() {
+
+}
