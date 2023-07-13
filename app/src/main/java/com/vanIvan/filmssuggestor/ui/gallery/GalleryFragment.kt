4d0b679bf7fdaeac93d4014ba7c948d18e1cff4c
Ivan Seregin <vanIvan4.work@gmail.com>
2023-07-13 11:58:01 +0500

    Update app

--- app/src/main/java/com/vanIvan/filmssuggestor/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/vanIvan/filmssuggestor/ui/gallery/GalleryFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.vanIvan.filmssuggestor.databinding.FragmentGalleryBinding
+import com.vanIvan.filmssuggestor.databinding.FragmentArchiveBinding
 
 class GalleryFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentArchiveBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class GalleryFragment : Fragment() {
         val galleryViewModel =
                 ViewModelProvider(this).get(GalleryViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentArchiveBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
